73320bd4142af1052a0d74195cb557f5d8d8d29c
XunJi <118670760+XunJiJiang@users.noreply.github.com>
2025-08-16 17:37:36 +0800

    feat: fetchImage 第二个参数修改为可选

--- server/utils/fetchImage.ts
+++ server/utils/fetchImage.ts
@@ -1,5 +1,8 @@
+import { createLogger } from './log';
+
 /** 传入一个图片url, 尝试访问并获取返回的数据 Buffer */
-export async function fetchImage(url: string, log: SCWC.Log): Promise<Buffer | null> {
+export async function fetchImage(url: string, log?: SCWC.Log): Promise<Buffer | null> {
+  log = log ? log : createLogger(`[fetchImage]`, url);
   try {
     const response = await fetch(url);
     if (!response.ok) throw new Error(`HTTP error! status: ${response.status}`);
