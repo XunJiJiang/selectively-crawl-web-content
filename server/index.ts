3ba346b898eac279a7f7d5d0a5ec57f1393c66d4
XunJi <118670760+XunJiJiang@users.noreply.github.com>
2026-03-08 17:45:46 +0800

    fix(server): 修复 SIGINT 信号会多次触发的问题

--- server/index.ts
+++ server/index.ts
@@ -29,8 +29,34 @@ listen(PORT, () => {
 // 监听命令行输入
 listenProcessStdin(serverLogger);
 
+/** 退出信号发生次数 */
+let sigintIsTriggered = 0;
+
+/*
+ * 关于这里为什么要在第三次 SIGINT 信号时才强制退出:
+ * 1. 第一次 SIGINT 信号: 触发正常的退出流程, 调用插件的 onUnload 方法, 进行资源清理等操作.
+ * 2. 第二次 SIGINT 信号: 由于未知问题，如果第一次 SIGINT 信号是由 ctrl+c 触发的
+ *    若30ms内没有完成退出流程, 则会自动再次触发 SIGINT 信号, 这个 SIGINT 信号不是用户按下 ctrl+c 触发的
+ *    这时不应该再次执行退出流程, 以免造成重复调用插件的 onUnload 方法等问题
+ *    也不应该直接强制退出, 因为可能第一次 SIGINT 信号触发的退出流程还没有完成
+ * 3. 第三次 SIGINT 信号: 此时的 SIGINT 信号一定是用户再次按下 ctrl+c 触发的, 这时才应该强制退出, 不执行退出处理器, 直接退出程序
+ *
+ * 关于为什么会有第二次 SIGINT 信号被自动触发的问题:
+ *    原因未知. 无法最简复现.
+ */
+
 // 监听退出信号
 process.on('SIGINT', async () => {
+  sigintIsTriggered++;
+  if (sigintIsTriggered > 2) {
+    // 立即退出, 不执行退出处理器
+    console.warn('强制退出');
+    process.exit(0);
+  }
+  if (sigintIsTriggered > 1) {
+    return;
+  }
+
   for (const plugin of plugins) {
     // TODO: 当实现重启后, 调用时需要告知插件本次关闭为重启
     if (plugin.handler && typeof plugin.handler.onUnload === 'function') {
