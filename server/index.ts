5438741ffceb6d4bff04e000b5bd2e559b6a5e97
XunJi <118670760+XunJiJiang@users.noreply.github.com>
2026-03-09 23:23:28 +0800

    fix: 使用新的方法修复 SIGINT 信号多次触发的问题 - 将不再需要第三次触发 ctrl+c 才能强制退出

--- server/index.ts
+++ server/index.ts
@@ -29,34 +29,49 @@ listen(PORT, () => {
 // 监听命令行输入
 listenProcessStdin(serverLogger);
 
-/** 退出信号发生次数 */
-let sigintIsTriggered = 0;
-
 /*
- * 关于这里为什么要在第三次 SIGINT 信号时才强制退出:
- * 1. 第一次 SIGINT 信号: 触发正常的退出流程, 调用插件的 onUnload 方法, 进行资源清理等操作.
- * 2. 第二次 SIGINT 信号: 由于未知问题，如果第一次 SIGINT 信号是由 ctrl+c 触发的
- *    若30ms内没有完成退出流程, 则会自动再次触发 SIGINT 信号, 这个 SIGINT 信号不是用户按下 ctrl+c 触发的
- *    这时不应该再次执行退出流程, 以免造成重复调用插件的 onUnload 方法等问题
- *    也不应该直接强制退出, 因为可能第一次 SIGINT 信号触发的退出流程还没有完成
- * 3. 第三次 SIGINT 信号: 此时的 SIGINT 信号一定是用户再次按下 ctrl+c 触发的, 这时才应该强制退出, 不执行退出处理器, 直接退出程序
+ * 当使用 npm run start 或 bun run start 启动时, 存在问题:
+ *   当用户按下 ctrl+c 时, 会触发两次 SIGINT 信号
+ * 直接使用 tsx 或 bun 运行时, 则不会有第二次 SIGINT 信号被自动触发的问题, 可以正常在第一次 SIGINT 信号时退出.
+ * 可能因为 bun、npm、yarn 等间接启动方式会通过 npm 的子进程机制（spawn）启动脚本,
+ * npm（或 bun）作为父进程，收到 ctrl+c 后，会向所有子进程发送 SIGINT，但有时会因为进程管理或事件冒泡机制，导致 SIGINT 被触发两次。
+ */
+
+/**
+ * 注意: 下面的解决办法可能并不属于官方行为, 可能在未来的版本中会被修改
+ * 确认生效版本: bun v1.3.6; node v22.16.0 - v25.8.0
+ * 现在的解决办法:
+ * 1. 实际处理退出逻辑的 SIGINT 监听器使用 process.once 来确保只执行一次
+ * 2. 此时用户按下 ctrl+c 后, 仍然会触发两次 SIGINT 信号, 导致立刻退出并输出 'error: script "start" exited with code 130'
+ *    通过添加一个空的 SIGINT 监听器可以避免多次触发导致的立刻退出, 因为没有立刻退出, 也不会输出 'error: script "start" exited with code 130'
+ * 3. 但此时会报 'Error: read EIO' 错误, 而且这个错误不会正常显示在控制台, 而是按 'up' 键尝试输入上次的命令时才会显示
+ *    例如, 上次命令 'bun start', 在结束后不会显示错误, 但当按 'up' 键时会显示下面内容
+ *    可以看到上次实际上的命令 'bun start' 被接在了错误信息的后面
+ *    这并不会影响正常使用, 但看起来不太好
+ * 4. 通过捕获 stdin 的 error 事件, 可以捕获到这个错误, 并且可以判断如果错误码是 EIO 就忽略掉, 这样就不会在按 'up' 键时显示这个错误了
+ *```bash
+ *❯ node:events:486
+ *      throw er; // Unhandled 'error' event
+ *      ^
  *
- * 关于为什么会有第二次 SIGINT 信号被自动触发的问题:
- *    原因未知. 无法最简复现.
+ *Error: read EIO
+ *    at TTY.onStreamRead (node:internal/stream_base_commons:216:20)
+ *Emitted 'error' event on ReadStream instance at:
+ *    at emitErrorNT (node:internal/streams/destroy:170:8)
+ *    at emitErrorCloseNT (node:internal/streams/destroy:129:3)
+ *    at process.processTicksAndRejections (node:internal/process/task_queues:90:21) {
+ *  errno: -5,
+ *  code: 'EIO',
+ *  syscall: 'read'
+ *}
+ *
+ *Node.js v25.8.0
+ *bun start
+ *```
  */
 
 // 监听退出信号
-process.on('SIGINT', async () => {
-  sigintIsTriggered++;
-  if (sigintIsTriggered > 2) {
-    // 立即退出, 不执行退出处理器
-    console.warn('强制退出');
-    process.exit(0);
-  }
-  if (sigintIsTriggered > 1) {
-    return;
-  }
-
+process.once('SIGINT', async () => {
   for (const plugin of plugins) {
     // TODO: 当实现重启后, 调用时需要告知插件本次关闭为重启
     if (plugin.handler && typeof plugin.handler.onUnload === 'function') {
@@ -67,5 +82,23 @@ process.on('SIGINT', async () => {
 
   const log = createLogger('server', `http://localhost:${PORT}`);
   log.pathInfo(`服务停止`);
+
   process.exit(0);
+});
+
+/** SIGINT 触发次数 */
+let sigintCount = 0;
+
+// 监听 SIGINT 信号, 实现再次按下 ctrl+c 时强制退出
+process.on('SIGINT', () => {
+  sigintCount++;
+});
+
+process.stdin.on('error', err => {
+  if (sigintCount > 0 && 'code' in err && err.code === 'EIO') {
+    // 这是在某些环境（如 Windows）中按下 Ctrl+C 时 stdin 关闭导致的错误，可以安全忽略
+    return;
+  }
+
+  console.error(err);
 });
