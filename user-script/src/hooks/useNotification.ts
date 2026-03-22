f00a7971c8f822a00908a0dd736e87343667fb04
XunJi <118670760+XunJiJiang@users.noreply.github.com>
2026-03-22 14:52:50 +0800

    feat: 修改通知框的默认关闭时间为5s

--- user-script/src/hooks/useNotification.ts
+++ user-script/src/hooks/useNotification.ts
@@ -1,13 +1,40 @@
 import { notification } from 'antd';
 
-export function useNotification() {
+/**
+ * 没有自定义关闭延迟则修改为 5s
+ */
+function customNotify(args: Parameters<typeof notification.info>): Parameters<typeof notification.info> {
+  if (args.length !== 1) {
+    return args;
+  }
+  const firstArg = args[0];
+  if (typeof firstArg === 'object' && !('duration' in firstArg)) {
+    return [
+      {
+        ...firstArg,
+        duration: 5,
+      },
+      ...args.slice(1),
+    ] as Parameters<typeof notification.info>;
+  }
+  return args;
+}
+
+export function useNotification(): {
+  api: ReturnType<typeof notification.useNotification>[0];
+  contextHolder: ReturnType<typeof notification.useNotification>[1];
+  info: typeof notification.info;
+  success: typeof notification.success;
+  error: typeof notification.error;
+  warn: typeof notification.warning;
+} {
   const [api, contextHolder] = notification.useNotification();
   return {
     api,
     contextHolder,
-    info: notification.info,
-    success: notification.success,
-    error: notification.error,
-    warn: notification.warning,
+    info: (...args) => notification.info(...customNotify(args)),
+    success: (...args) => notification.success(...customNotify(args)),
+    error: (...args) => notification.error(...customNotify(args)),
+    warn: (...args) => notification.warning(...customNotify(args)),
   };
 }
