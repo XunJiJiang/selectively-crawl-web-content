f33e1d69a327034ec6191db6f3ce3983579ca828
XunJi <118670760+XunJiJiang@users.noreply.github.com>
2026-05-08 12:31:39 +0800

    feat(server.command): 用于优化命令输出的函数声明

--- server/utils/log.ts
+++ server/utils/log.ts
@@ -39,3 +39,11 @@ export function createLogger (tag: string, relativePath: string) {
 }
 
 export default log;
+
+/**
+ * 为 command 提供的函数, 用于记录当前用户输入的命令和参数
+ * 以便控制台输出时不覆盖当前输入的命令
+ * @param nowInput 当前输入的命令和参数字符串
+ * @param predictInput 预测的命令和参数字符串, 用于提示用户可能的输入, 必须以 nowInput 开头, 可以与 nowInput 完全相同
+ */
+export function logCommandInput (nowInput: string, predictInput?: string) {}
