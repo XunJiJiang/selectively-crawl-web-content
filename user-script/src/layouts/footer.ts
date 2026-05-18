d8b3c0c373e8b1889b4f72636b7dee05cecdf892
xunjijiang <329453626x@gmail.com>
2026-05-18 11:32:55 +0800

    feat(user-script): 设置初始值

--- user-script/src/layouts/footer.ts
+++ user-script/src/layouts/footer.ts
@@ -2,15 +2,15 @@ import style from './footer.css?raw';
 
 import { LitElement, html, css, unsafeCSS } from 'lit';
 import { customElement, property } from 'lit/decorators.js';
-import { configContext, type TConfig } from '../store/config';
+import { config, configContext, type TConfig } from '../store/config';
 import { consume } from '@lit/context';
 
 @customElement('scwc-layout-footer')
 export class SCWCFooterLayout extends LitElement {
   static styles = [css`${unsafeCSS(style)}`];
 
-  @consume({ context: configContext, subscribe: true, })
-  private accessor config: TConfig;
+  @consume({ context: configContext, subscribe: true })
+  private accessor config: TConfig = config;
 
   render () {
     return html``;
--- user-script/src/layouts/header.ts
+++ user-script/src/layouts/header.ts
@@ -3,14 +3,14 @@ import style from './header.css?raw';
 import { LitElement, html, css, unsafeCSS } from 'lit';
 import { customElement, property } from 'lit/decorators.js';
 import { consume } from '@lit/context';
-import { configContext, type TConfig } from '../store/config'
+import { config, configContext, type TConfig } from '../store/config'
 
 @customElement('scwc-layout-header')
 export class SCWCHeaderLayout extends LitElement {
   static styles = [css`${unsafeCSS(style)}`];
 
-  @consume({ context: configContext, subscribe: true, })
-  private accessor config: TConfig;
+  @consume({ context: configContext, subscribe: true })
+  private accessor config: TConfig = config;
 
   render () {
     return html``;
