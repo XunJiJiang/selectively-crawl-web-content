fc329ab8019901038253028abbc9f2f4dd80de0a
XunJi <118670760+XunJiJiang@users.noreply.github.com>
2026-05-20 00:56:27 +0800

    feat(user-script): button 组件

---
+++ user-script/src/components/button.ts
@@ -0,0 +1,42 @@
+import style from './button.css?raw';
+
+import { LitElement, html, css, unsafeCSS } from 'lit';
+import { customElement, property } from 'lit/decorators.js';
+import { classMap } from 'lit/directives/class-map.js';
+
+@customElement('scwc-button')
+export class SCWCContentLayout extends LitElement {
+  static styles = [css`${unsafeCSS(style)}`];
+
+  @property({ type: String })
+  accessor label = '';
+
+  @property({ type: Boolean })
+  accessor disabled = false;
+
+  @property({ type: String })
+  accessor type: 'primary' | 'secondary' | 'inline' = 'primary';
+
+  render () {
+    return html`
+      <button
+        class=${[classMap({
+      'scwc-btn-primary': this.type === 'primary',
+      'scwc-btn-secondary': this.type === 'secondary',
+      'scwc-btn-inline': this.type === 'inline',
+      'scwc-btn-disabled': this.disabled,
+    }), 'scwc-btn']}
+        ?disabled=${this.disabled}
+        @click=${() => this.dispatchEvent(new CustomEvent('click', { bubbles: true }))}
+      >
+        ${this.label}
+      </button>
+    `;
+  }
+}
+
+declare global {
+  interface HTMLElementTagNameMap {
+    "scwc-button": SCWCContentLayout;
+  }
+}
