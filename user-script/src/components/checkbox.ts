47d117c4c0cca1c061ced8e4afd0492d066b8947
XunJi <118670760+XunJiJiang@users.noreply.github.com>
2026-05-21 01:54:47 +0800

    feat(user-script): checkbox 组件

--- user-script/src/components/checkbox.ts
+++ user-script/src/components/checkbox.ts
@@ -2,17 +2,49 @@ import style from './checkbox.css?raw';
 
 import { LitElement, html, css, unsafeCSS } from 'lit';
 import { customElement, property } from 'lit/decorators.js';
+import { classMap } from 'lit/directives/class-map.js';
+import { v4 as uuidv4 } from 'uuid';
 
 @customElement('scwc-checkbox')
 export class SCWCContentLayout extends LitElement {
   static styles = [css`${unsafeCSS(style)}`];
 
+  @property({ type: Boolean })
+  accessor checked = false;
+
+  @property({ type: Boolean })
+  accessor disabled = false;
+
+  @property({ type: String })
+  accessor label = '';
+
+  @property({ type: String })
+  accessor id = `scwc-checkbox-${uuidv4()}`;
+
   render () {
-    return html``;
+    return html`
+      <label for=${this.id} class="scwc-checkbox-label">
+        <input
+          class=${classMap({
+      'scwc-checkbox': true,
+      'scwc-checkbox-disabled': this.disabled,
+    })}
+          id=${this.id}
+          type="checkbox"
+          .checked=${this.checked}
+          ?disabled=${this.disabled}
+          @change=${(e: Event) => {
+        this.dispatchEvent(new CustomEvent('change', {
+          detail: (e.target as HTMLInputElement).checked,
+          bubbles: true,
+        }));
+      }}
+        />
+        <span class="scwc-checkbox-desc">${this.label}</span>
+      </label>
+    `;
   }
 }
-
-
 
 declare global {
   interface HTMLElementTagNameMap {
