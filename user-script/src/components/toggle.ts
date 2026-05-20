250456abf382fdd9b7a74340aaa8eddb50927652
XunJi <118670760+XunJiJiang@users.noreply.github.com>
2026-05-21 02:03:28 +0800

    feat(user-script): toggle 组件

--- user-script/src/components/toggle.ts
+++ user-script/src/components/toggle.ts
@@ -2,13 +2,39 @@ import style from './toggle.css?raw';
 
 import { LitElement, html, css, unsafeCSS } from 'lit';
 import { customElement, property } from 'lit/decorators.js';
+import { v4 as uuidv4 } from 'uuid';
 
 @customElement('scwc-toggle')
 export class SCWCContentLayout extends LitElement {
   static styles = [css`${unsafeCSS(style)}`];
 
+  @property({ type: Boolean })
+  accessor checked = false;
+
+  @property({ type: Boolean })
+  accessor disabled = false;
+
+  @property({ type: String })
+  accessor label = '';
+
+  @property({ type: String })
+  accessor id = `scwc-toggle-${uuidv4()}`;
+
   render () {
-    return html``;
+    return html`
+      <scwc-checkbox
+        id=${this.id}
+        .checked=${this.checked}
+        ?disabled=${this.disabled}
+        .label=${this.label}
+        @change=${(e: CustomEvent<boolean>) => {
+        this.dispatchEvent(new CustomEvent('change', {
+          detail: e.detail,
+          bubbles: true,
+        }));
+      }}
+      ></scwc-checkbox>
+    `;
   }
 }
 
