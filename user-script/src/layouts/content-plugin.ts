65c0b62bc32f9c8949a3ce8f3da700c51cf097f6
XunJi <118670760+XunJiJiang@users.noreply.github.com>
2026-05-25 14:30:32 +0800

    feat(user-script): 插件部分添加参数

--- user-script/src/layouts/content-plugin.ts
+++ user-script/src/layouts/content-plugin.ts
@@ -4,6 +4,7 @@ import { LitElement, html, css, unsafeCSS } from 'lit';
 import { customElement, property } from 'lit/decorators.js';
 import { config, configContext, type TConfig } from '../store/config';
 import { consume } from '@lit/context';
+import type { Item } from '../types/claw';
 
 @customElement('scwc-layout-content-plugin')
 export class SCWCContentPlugin extends LitElement {
@@ -11,6 +12,12 @@ export class SCWCContentPlugin extends LitElement {
 
   @consume({ context: configContext, subscribe: true })
   private accessor config: TConfig = config;
+
+  @property({ type: Boolean })
+  accessor expanded: boolean = false;
+
+  @property({ type: Array, attribute: false })
+  accessor clawItems: Item[] = [];
 
   render () {
     return html``;
