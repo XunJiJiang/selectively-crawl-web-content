05ca759d2f01fc979288f02a657615ec26faac14
XunJi <118670760+XunJiJiang@users.noreply.github.com>
2026-05-25 13:20:54 +0800

    perf(user-script): 修改数组类型

--- user-script/src/types/utils.d.ts
+++ user-script/src/types/utils.d.ts
@@ -1,7 +1,7 @@
 /** 可以被 JSON 序列化的类型 */
 type JSONPrimitive = string | number | boolean | null;
 export interface JSONObject { [key: string]: JSONValue; }
-export type JSONValue = JSONPrimitive | JSONObject | Array<JSONValue>;
+export type JSONValue = JSONPrimitive | JSONObject | JSONValue[];
 
 /** 可以被 JSON 序列化的类型, 但是将数组替换为函数 */
 export interface JSONObjectWithFunction { [key: string]: JSONValueWithFunction; }
@@ -14,7 +14,7 @@ export type ArrayProcessor<T = any> = (item?: JSONValue) => item extends JSONVal
 /** 将 JSONValue 转换为 JSONValueWithFunction */
 export type JSONValueToFunction<T extends JSONValue> = T extends JSONPrimitive
   ? T
-  : T extends Array<infer U>
+  : T extends (infer U)[]
   ? ArrayProcessor<U>
   : T extends JSONObject
   ? { [K in keyof T]: JSONValueToFunction<T[K]> }
@@ -33,9 +33,9 @@ export type ResolvedJSONValue<T extends JSONValueWithFunction> = T extends JSONP
 // export type MergedJSONValue<T extends JSONValue, U extends JSONValueWithFunction, KEEPEXTRA extends boolean> =
 //   T extends JSONPrimitive
 //   ? ResolvedJSONValue<U>
-//   : T extends Array<infer TItem>
+//   : T extends (infer TItem)[]
 //   ? U extends ArrayProcessor<infer UItem>
-//     ? Array<MergedJSONValue<TItem, UItem, KEEPEXTRA>>
+//     ? (MergedJSONValue<TItem, UItem, KEEPEXTRA>)[]
 //     : U
 //   : T extends JSONObject
 //     ? U extends JSONObjectWithFunction
