af2f725ba069329c75223604ccba89b53da06005
XunJi <118670760+XunJiJiang@users.noreply.github.com>
2026-06-28 23:37:31 +0800

    feat(server): 导出根目录路径; 解析环境变量

--- projects/server/common/env.ts
+++ projects/server/common/env.ts
@@ -1,8 +1,19 @@
 import dotenv from 'dotenv';
+import path from 'node:path';
 import { serverLogger } from './logger.ts';
 import { v4 } from 'uuid';
 
-dotenv.config();
+const __dirname = import.meta.dirname;
+
+/** 所有项目的根目录 */
+export const ROOT = path.join(__dirname, '..', '..', '..');
+
+/** 当前程序启动文件所在目录 */
+export const SERVER_ROOT = path.join(__dirname, '..');
+
+dotenv.config({
+  path: path.join(ROOT, '.env'),
+});
 
 export const PORT = process.env.PORT ? Number(process.env.PORT) : 3200;
 export const HOST = process.env.HOST ?? 'http://localhost';
