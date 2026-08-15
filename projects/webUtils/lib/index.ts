8080799fb0b3e8686bac5f789fb454af7691a210
XunJi <118670760+XunJiJiang@users.noreply.github.com>
2026-08-16 02:27:44 +0800

    fix(webUtils): 修复在独立打开插件页面时无法初始化的问题

--- projects/webUtils/lib/index.ts
+++ projects/webUtils/lib/index.ts
@@ -1,4 +1,7 @@
-import type { TConfig } from '../../shared/store/config.ts';
+import { defaultValue, type TConfig } from '../../shared/store/config.ts';
+import type { JSONValueWithFunction } from '../../shared/types/utils';
+import { CONFIG_KEY } from '../../shared/utils/common.ts';
+import { loadFromStorage } from '../../shared/utils/storage.ts';
 import { createFetch, type TFetch } from './utils/fetch.ts';
 
 // TODO: 动态更新配置
@@ -6,10 +9,12 @@ import { createFetch, type TFetch } from './utils/fetch.ts';
 // TODO: 单独打开子页面时从 localStorage 获取配置, 此时不允许隐藏子页面 SCWC 窗口
 
 export type TSCWCUtils = {
+  hasInitialized: boolean;
   fetch: TFetch;
 };
 
 const scwcutils: TSCWCUtils = {
+  hasInitialized: false,
   fetch: (() => {
     console.warn('scwcutils.fetch 未初始化, 请在页面加载完成后再使用 scwcutils.fetch');
   }) as unknown as TFetch,
@@ -23,9 +28,30 @@ window.addEventListener('message', (event) => {
   if (message.type !== 'scwc-plugin-config') {
     return;
   }
+  // 以父页面的配置为准
+  // 即使 scwcutils 已经初始化过了, 也要重新初始化
   createFetch(message.config).then((fetch) => {
     scwcutils.fetch = fetch;
+    scwcutils.hasInitialized = true;
   });
+});
+
+// 在没有父页面的情况下, 直接从 localStorage 获取配置并初始化
+// 只在页面加载完成后初始化一次, 不监听配置变化
+// 当配置变化后, 需要刷新页面才能生效
+window.addEventListener('load', () => {
+  // 如果 scwcutils 已经初始化过了, 则不再重复初始化
+  if (!scwcutils.hasInitialized) {
+    const config: TConfig = loadFromStorage(
+      CONFIG_KEY,
+      defaultValue as unknown as JSONValueWithFunction,
+      'a',
+    );
+    createFetch(config).then((fetch) => {
+      scwcutils.fetch = fetch;
+      scwcutils.hasInitialized = true;
+    });
+  }
 });
 
 export default scwcutils;
